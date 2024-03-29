06f81c45111523bcec297d920de301419de34567
Ez4LTarmy <97959351+Ez4LTarmy@users.noreply.github.com>
2024-03-30 03:58:39 +0800

    Create MapnominatedCommand.cs

---
+++ Features/MapnominatedCommand.cs
@@ -0,0 +1,15 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Entities;
+using cs2_rockthevote.Core;
+
+namespace cs2_rockthevote
+{
+    public partial class Plugin
+    {
+        [ConsoleCommand("mapnominated", "Display nominated maps")]
+        public void OnMapNominated(CCSPlayerController player, CommandInfo command)
+        {
+            _mapLister.PrintNominatedMapsToClient(player);
+        }
+    }
+}
