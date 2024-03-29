2c345a6af8e9c8ef8104dee37ae0b24b6b5dccd2
Ez4LTarmy <97959351+Ez4LTarmy@users.noreply.github.com>
2024-03-30 04:16:43 +0800

    Update MapnominatedCommand.cs

--- Features/MapnominatedCommand.cs
+++ Features/MapnominatedCommand.cs
@@ -7,6 +7,13 @@ namespace cs2_rockthevote
 {
     public partial class Plugin
     {
+        private readonly MapLister _mapLister;
+
+        public Plugin(MapLister mapLister)
+        {
+            _mapLister = mapLister;
+        }
+
         [ConsoleCommand("mapnominated", "Display nominated maps")]
         public void OnMapNominated(CCSPlayerController player, CommandInfo command)
         {
