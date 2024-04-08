90e594fd67d5aadb753b5fe584cbba9e7fdf8ff3
Ez4LTarmy <97959351+Ez4LTarmy@users.noreply.github.com>
2024-04-09 06:59:57 +0800

    updated !nomlist

--- Features/MapnominatedCommand.cs
+++ Features/MapnominatedCommand.cs
@@ -4,24 +4,25 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
 
+
 public partial class Plugin
 {
+    public char NewLine = '\u2029';
     [ConsoleCommand("nomlist", "Show nomlist")]
     public void OnNomlist(CCSPlayerController? player, CommandInfo command)
     {
+
         var Nomlist = _nominationManager.Nomlist
             .Values
             .SelectMany(list => list)
             .Distinct()
             .Select((map, index) => $"{index + 1}. {map}");
 
-        player.PrintToChat("==> Nomlist will display on console <==");
+        string Maplist = string.Join(NewLine.ToString(), Nomlist);
 
-        string Maplist = string.Join(System.Environment.NewLine, Nomlist);
-
-        player.PrintToConsole("**** LIST OF MAPS NOMINATED ***");
-        player.PrintToConsole(Maplist);
-        player.PrintToConsole("*********************************");
+        player.PrintToChat("*** LIST MAP NOMINATED ***");
+        player.PrintToChat(Maplist);
+        player.PrintToChat("********************************");
     }
 
 }
