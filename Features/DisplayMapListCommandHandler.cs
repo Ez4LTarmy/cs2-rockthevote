09c3e2b3f397148d1bde3d166519a8b7822c6619
Oleg Yudashkin <tkurumi901@gmail.com>
2024-06-09 15:24:26 +0300

    Add DisplayMapListCommandHandler and update Plugin class
    Added a new class `DisplayMapListCommandHandler` in the `cs2_rockthevote.Features` namespace. This class implements the `IPluginDependency<Plugin, Config>` interface and handles the display of a map list from "maplist.txt". It also provides a "maplist" command for players to display a portion of the map list in the console.
    
    Updated the `Plugin` class in the `cs2_rockthevote` namespace to include a new private readonly field `_displayMapListCommandHandler` of type `DisplayMapListCommandHandler`. The `Plugin` class constructor now takes an additional parameter of type `DisplayMapListCommandHandler`, allowing the `Plugin` class to utilize the functionality provided by the `DisplayMapListCommandHandler` class.

---
+++ Features/DisplayMapListCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace cs2_rockthevote.Features;
+
+using cs2_rockthevote.Contracts;
+using cs2_rockthevote.Core;
+
+public class DisplayMapListCommandHandler : IPluginDependency<Plugin, Config>
+{
+    private IEnumerable<Map> _maps = Enumerable.Empty<Map>();
+    private readonly int _mapsPerPage = 25;
+
+    public void OnLoad(Plugin plugin)
+    {
+        var maplist = File.ReadAllLines("maplist.txt");
+        _maps = maplist.Select(map =>
+        {
+            var mapWithId = map.Split(':');
+
+            var name = mapWithId[0].AsSpan().Trim().ToString();
+            var id = mapWithId[1].AsSpan().Trim().ToString();
+
+            return new Map(name, id);
+        });
+
+        plugin.AddCommand("maplist", "Displays maplist into console", (player, info) =>
+        {
+            var part = info.GetArg(0); // current part to display
+
+            if (!int.TryParse(part, out var partNumber))
+            {
+                player?.PrintToConsole("You need to provide the number which part you want to display.");
+            }
+
+            _maps.Skip(_mapsPerPage * partNumber).Take(_mapsPerPage).ToList().ForEach(map =>
+            {
+                player?.PrintToConsole(map.Name);
+            });
+        });
+    }
+}
